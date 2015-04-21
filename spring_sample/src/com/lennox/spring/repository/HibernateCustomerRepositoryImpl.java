d4c9a48abd1f87d9302f711a12d33ce172e870a6
ajayindfw <ajayindfw@gmail.com>
2015-04-21 06:39:51 -0500

    Added annotation based configuration support. Had to compile and run it back to JDK 7 to avoid ARM related error messages with classes compiled with JDK 8.

--- spring_sample/src/com/lennox/spring/repository/HibernateCustomerRepositoryImpl.java
+++ spring_sample/src/com/lennox/spring/repository/HibernateCustomerRepositoryImpl.java
@@ -3,8 +3,11 @@ package com.lennox.spring.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import com.lennox.spring.model.Customer;
 
+@Repository("customerRepository")
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 	/* (non-Javadoc)
 	 * @see com.lennox.spring.repository.CustomerRepository#findAll()
--- spring_sample/src/com/lennox/spring/service/CustomerServiceImpl.java
+++ spring_sample/src/com/lennox/spring/service/CustomerServiceImpl.java
@@ -2,16 +2,22 @@ package com.lennox.spring.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.lennox.spring.repository.CustomerRepository;
 import com.lennox.spring.model.Customer;
 
+@Service("customerService")
 public class CustomerServiceImpl implements CustomerService {
+	
 	private CustomerRepository customerRepository;
 	
 	public CustomerServiceImpl() {
 		
 	}
 	
+	@Autowired
 	public CustomerServiceImpl (CustomerRepository customerRepository) {
 		this.customerRepository = customerRepository;
 	}
